6504e0ce39da4c4ffbb6a24cef8a649d35eb7021
notangelmario <savin@angelmario.eu>
2022-10-01 01:40:16 +0300

    Init project

---
+++ src/lib/components/Editor.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { basicSetup } from "codemirror";
+	import { EditorView } from "@codemirror/view";
+	import { EditorState } from "@codemirror/state";
+	import { javascript } from "@codemirror/lang-javascript";
+	import { onMount } from "svelte";
+
+	let editorWrapper;
+
+	onMount(() => {
+		let editor = new EditorView({
+			parent: editorWrapper,
+			extensions: [
+				basicSetup,
+				javascript(),
+				EditorState.tabSize.of(4),
+			]
+		})
+	})
+</script>
+
+<div bind:this={editorWrapper} class="editor-wrapper"></div>
+
+
+<style scoped>
+	.editor-wrapper {
+		width: 100%;
+		height: 100%
+	}
+</style>
