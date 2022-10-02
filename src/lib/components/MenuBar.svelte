23a2152501c2464b2db88581985b3d9bb5ba2e35
notangelmario <savin@angelmario.eu>
2022-10-02 12:10:29 +0300

    Updated menubar

--- src/lib/components/MenuBar.svelte
+++ src/lib/components/MenuBar.svelte
@@ -17,17 +17,69 @@
 	function saveFileClick() {
 		writeFile($fileHandleStore, $editor.state.doc as unknown as string)
 	}
+
+	function closeFileClick() {
+		$editor.dispatch({ changes: { from:0, empty: true, to: $editor.state.doc.length } });
+
+		fileHandleStore.set(undefined);
+	}
 </script>
 
-<div class="menuBarWrapper">
-	<button on:click={openFileClick}>Open File</button>
-	<button on:click={saveFileClick} disabled={!$fileHandleStore}>Save File</button>
-</div>
+<ul class="menuBarWrapper">
+	<li>
+		<span>
+			File
+		</span>
+		<div>
+			<button on:click={openFileClick}>Open File...</button>
+			<button on:click={saveFileClick} disabled={!$fileHandleStore}>Save File</button>
+			<button on:click={closeFileClick} disabled={!$fileHandleStore}>Close File</button>
+		</div>
+	</li>
+</ul>
 
 <style scoped>
 	.menuBarWrapper {
 		display: flex;
 		flex-flow: row;
 		flex-wrap: nowrap;
+	}
+
+	.menuBarWrapper li > span {
+		padding: .25rem .5rem;
+		cursor: default;
+		background-color: #fff;
+	}
+
+	.menuBarWrapper li > span:hover {
+		filter: brightness(.9);
+	}
+
+	.menuBarWrapper li:hover > div {
+		display: flex;
+	}
+
+	.menuBarWrapper li > div {
+		position: absolute;
+		display: none;
+		z-index: 999;
+		flex-flow: column;
+		background-color: #fff;
+		padding: .5rem 0;
+		outline: 1px solid #000;
+		border-radius: 0 .5rem .5rem .5rem;
+	}
+
+	button {
+		background-color: #fff;
+		outline: none;
+		border: none;
+		padding: .25rem .5rem;
+		width: 10rem;
+		text-align: left;
+	}
+
+	button:hover {
+		filter: brightness(.9);
 	}
 </style>
