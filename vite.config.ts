62f572831eb1231670d1221378df834867e83082
notangelmario <savin@angelmario.eu>
2022-10-08 00:52:17 +0300

    Made register type to prompt

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         enabled: false // Enable install on dev mode
       },
       injectRegister: null,
-      registerType: "autoUpdate",
+      registerType: "prompt",
       workbox: {
         cleanupOutdatedCaches: true,
         maximumFileSizeToCacheInBytes: 30000000
