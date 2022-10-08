bd4622019895f68fc690395b6e9759a1488799a1
notangelmario <savin@angelmario.eu>
2022-10-08 12:44:57 +0300

    Added compatibility checking
    Fixed dark mode on startup background color

--- src/main.ts
+++ src/main.ts
@@ -6,36 +6,43 @@ import { initEditor } from "./lib/editor";
 import { closeModal, openModal } from "./lib/modal";
 import { initLanguageWorkers } from "./lib/languages";
 import { createNotice, initStatus } from "./lib/status";
+import { isCompat } from "./lib/compat";
 
 const editorWrapper = document.getElementById("app")!;
 
-const store = initStore();
-initLanguageWorkers();
-const editor = initEditor(editorWrapper, store);
-initStatus(editor);
+if (isCompat()) {
+	const store = initStore();
+	initLanguageWorkers();
+	const editor = initEditor(editorWrapper, store);
+	initStatus(editor);
 
-registerSW({
-  onNeedRefresh() {
-  	createNotice("Pencil will update at the next restart");
-  },
-  onOfflineReady() {
-  	createNotice("Pencil is ready to be used offline");
-  },
-})
+	registerSW({
+	  onNeedRefresh() {
+	  	createNotice("Pencil will update at the next restart");
+	  },
+	  onOfflineReady() {
+	  	createNotice("Pencil is ready to be used offline");
+	  },
+	})
 
-// Handles tutorial
-if (!store.get("visited")) {
-	openModal(editor, { autoHide: false, text: "Press Ctrl+P to get started" })
+	// Handles tutorial
+	if (!store.get("visited")) {
+		openModal(editor, { autoHide: false, text: "Press Ctrl+P to get started" })
 
-	const triggered = editor.createContextKey<boolean>("intro-triggered", false);
-	
-	editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyP, () => {
-		triggered.set(true);
-		store.set("visited", true)
+		const triggered = editor.createContextKey<boolean>("intro-triggered", false);
+		
+		editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyP, () => {
+			triggered.set(true);
+			store.set("visited", true)
 
-		editor.trigger("ctrl+p", "editor.action.quickCommand", null);
+			editor.trigger("ctrl+p", "editor.action.quickCommand", null);
 
-		closeModal(editor);
-		openModal(editor, { autoHide: true, text: "This is the Command Palette! This is how you get around Pencil! Have fun!" })
-	}, "!intro-triggered")
+			closeModal(editor);
+			openModal(editor, { autoHide: true, text: "This is the Command Palette! This is how you get around Pencil! Have fun!" })
+		}, "!intro-triggered")
+	}
+} else {
+	const unsupportedWrapper = document.getElementById("unsupported")!;
+	editorWrapper.style.display = "none";
+	unsupportedWrapper.style.display = "block";
 }
